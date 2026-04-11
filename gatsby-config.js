3f15f4c96cbaaa92ecd5931c8e0c06c6b518bf61
raphael-ms <raphaelmarques77@gmail.com>
2026-04-11 16:18:13 +0100

    feat: Add Google Analytics 4 tracking with gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,11 +21,29 @@ module.exports = {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0Z5YJD8CL6", // Replace with your Google Analytics 4 Measurement ID
+        ],
+        // This object gets passed directly to the gtag config command
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     "gatsby-plugin-sitemap",
